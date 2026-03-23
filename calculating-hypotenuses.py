c815f40858e92854cee069f25c18798ec0bfdccb
Carlos Arthur <arthurhollyfild@gmail.com>
2026-03-23 19:34:06 -0300

    Update calculating-hypotenuses.py

--- calculating-hypotenuses.py
+++ calculating-hypotenuses.py
@@ -2,3 +2,6 @@ import math
 name=input('Seja bem vindo(a)! Como você se chama? ')
 print('É um prazer te cohecer {}!'.format(name))
 cato=float((input('Qual a medida do seu cateto oposto em cm? ')))
+cata=float(input('Qual a medida do cateto adjacente em cm? '))
+hip=math.hypot(cato,cata)
+print('{}, a hipotenusa do seu triângulo corresponde a: {}'.format(name,hip))
