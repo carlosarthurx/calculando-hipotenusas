a40c5503828d376315d75fc62bca669ed957f7c4
Carlos Arthur <arthurhollyfild@gmail.com>
2026-03-23 19:33:26 -0300

    Create calculating-hypotenuses.py

---
+++ calculating-hypotenuses.py
@@ -0,0 +1,4 @@
+import math
+name=input('Seja bem vindo(a)! Como você se chama? ')
+print('É um prazer te cohecer {}!'.format(name))
+cato=float((input('Qual a medida do seu cateto oposto em cm? ')))
